a867af92327c7fcd5b904a400e86e491403141ca
Miles Dragan <miloszdragan@openmailbox.org>
2015-09-29 16:45:45 +0200

    Style login and register forms

--- resources/views/forms/login.blade.php
+++ resources/views/forms/login.blade.php
@@ -1,37 +1,35 @@
   {!! Form::open(['route' => ['auth.login'], 'method' => 'POST', 'class' => 'form-horizontal']) !!}
-
-<div class="form-group">
-  <label for="email" class="col-md-2 control-label">E-mail: </label>
-  <div class="col-md-4">
-    <input id="email" type="email" name="email" required autofocus>
+<fieldset>
+  <div class="form-group">
+    <label for="email" class="col-lg-2 control-label">E-mail: </label>
+    <div class="col-lg-9">
+      <input class="form-control" type="email" name="email" required autofocus>
+    </div>
   </div>
-</div>
 
-<div class="form-group">
-  <label for="password" class="col-md-2 control-label">Password: </label>
-  <div class="col-md-4">
-    <input id="password" type="password" name="password" 
-          required>
-        </div>
+  <div class="form-group">
+    <label for="password" class="col-lg-2 control-label">Password: </label>
+    <div class="col-lg-9">
+      <input class="form-control" type="password" name="password" required>
+      <div class="checkbox">
+        <label>
+          <input type="checkbox" name="remember" value="remember"  checked>Remember me
+        </label>
       </div>
+    </div>
+  </div>
 
-      <div class="form-group">
-        <div class="col-md-offset-2">
-          <div class="checkbox">
-            <label>
-              <input type="checkbox" name="remember" value="remember"  checked>Remember me
-            </label>
+  <div class="form-group">
+    <div class="col-lg-offset-2 col-lg-9">
+      <button type="submit" class="btn btn-primary">Log In</button>
+      <div class="help-block"  id="toggle-login">
+        <h4> Don't have account yet?</h4>
+              <a class="" href="{!! route('auth.showRegister') !!}">
+                <h3>Create new account!</h3>
+              </a>
+            </h4>
           </div>
         </div>
       </div>
-
-      <div class="form-group">
-        <div class="col-md-offset-2 col-sm-4">
-          <input type="submit" value="Log In">
-          <p class="help-block">Don't have account yet? <b><a
-              class="text-success" href="{!! route('auth.showRegister') !!}">Create new
-              account</a></b></p>
-        </div>
-      </div>
-
-  {!! Form::close() !!}
+</fieldset>
+{!! Form::close() !!}
--- resources/views/forms/register.blade.php
+++ resources/views/forms/register.blade.php
@@ -1,41 +1,42 @@
     {!! Form::open(['route' => ['auth.register'], 'method' => 'POST', 'class' => 'form-horizontal']) !!}
 
 <div class="form-group">
-  <label for="name" class="col-md-2 control-label">Name: </label>
-  <div class="col-md-4">
-    <input id="name" type="name" name="name" required autofocus>
+  <label for="name" class="col-lg-2 control-label">Name: </label>
+  <div class="col-lg-9">
+    <input class="form-control" type="name" name="name" required autofocus>
   </div>
 </div>
 
 <div class="form-group">
-  <label for="email" class="col-md-2 control-label">E-mail: </label>
-  <div class="col-md-4">
-    <input id="email" type="email" name="email"
-            required>
+  <label for="email" class="col-lg-2 control-label">E-mail: </label>
+  <div class="col-lg-9">
+    <input class="form-control" type="email" name="email" required>
           </div>
         </div>
 
         <div class="form-group">
-          <label for="password" class="col-md-2 control-label">Password: </label>
-          <div class="col-md-4">
-            <input id="password" type="password" name="password"
-            required>
+          <label for="password" class="col-lg-2 control-label">Password: </label>
+          <div class="col-lg-9">
+            <input class="form-control" type="password" name="password" required>
           </div>
         </div>
 
         <div class="form-group">
-          <label for="password-repeat" class="col-md-2 control-label">Re-type password: </label>
-          <div class="col-md-4">
-            <input id="password-repeat" type="password" name="password_confirmation"
-            required>
+          <label for="password-repeat" class="col-lg-2 control-label">Re-type password: </label>
+          <div class="col-lg-9">
+            <input class="form-control" type="password" name="password_confirmation" required>
           </div>
         </div>
 
         <div class="form-group">
-          <div class="col-md-offset-2 col-sm-4">
-            <input type="submit" value="Create Account">
-            <p class="help-block">Alreade have an account? <b><a
-                class="text-success" href="{{ route('auth.showLogin') }}">Log In</a></b></p>
+          <div class="col-lg-offset-2 col-lg-9">
+            <button type="submit" class="btn btn-primary">Create Account</button>
+            <div class="help-block"  id="toggle-login">
+              <h4> Already have an account?</h4>
+                <a href="{!! route('auth.showLogin') !!}">
+                  <h3>Log In!</h3>
+                </a>
+            </div>
           </div>
         </div>
 
