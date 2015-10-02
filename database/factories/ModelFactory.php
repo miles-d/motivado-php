bb12b14aee38783e014794ad304e1256161c168f
Miles Dragan <miloszdragan@openmailbox.org>
2015-10-03 01:13:46 +0200

    Add seeders for users and tasks

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -4,18 +4,28 @@
 |--------------------------------------------------------------------------
 | Model Factories
 |--------------------------------------------------------------------------
-|
-| Here you may define all of your model factories. Model factories give
-| you a convenient way to create models for testing and seeding your
-| database. Just tell the factory how a default model should look.
-|
 */
 
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->email,
-        'password' => bcrypt(str_random(10)),
+        'password' => bcrypt('secret'),
         'remember_token' => str_random(10),
+        'notify' => '0',
+    ];
+});
+
+/**
+ * Note: it implies 10 users
+ * Note: it generates future-done tasks, which makes no sense
+ */
+$factory->define(App\Task::class, function (Faker\Generator $faker) {
+    return [
+        'user_id' => mt_rand(1, 10),
+        'description' => Crypt::encrypt($faker->sentence),
+        'motivation' => Crypt::encrypt($faker->sentence),
+        'due_date' => $faker->dateTimeBetween('-5 weeks', '5 weeks')->format('Y-m-d'),
+        'completed' => mt_rand(0, 1)
     ];
 });
